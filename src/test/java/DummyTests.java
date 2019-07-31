2d4a669fd7c2dfcc3c072a3da32d8d2ab04a935a
SophYo <sophiya.dimitrova@gmail.com>
2019-07-31 23:29:38 +0300

    created with the needed test methods but empty

---
+++ src/test/java/DummyTests.java
@@ -0,0 +1,19 @@
+import org.junit.Test;
+
+public class DummyTests {
+
+    @Test
+    public void dummyShouldLosesHealthAttacked() {
+        //todo
+    }
+
+    @Test
+    public void dummyShouldThrowExceptionIfAttacked() {
+        //todo
+    }
+
+    @Test
+    public void deadDummyXP() {
+        //todo
+    }
+}
