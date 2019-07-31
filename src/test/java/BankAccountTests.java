94aa9975cd11d500fb790e80ee0ddef1d1c7f923
SophYo <sophiya.dimitrova@gmail.com>
2019-07-31 22:57:20 +0300

    second method added

--- src/test/java/BankAccountTests.java
+++ src/test/java/BankAccountTests.java
@@ -10,4 +10,9 @@ public class BankAccountTests {
         Assert.assertEquals(50, account.getBalance(), 0.0);
     }
 
+    @Test(expected = IllegalArgumentException.class)
+    public void depositWithNegativeValueShouldThrowException() {
+        BankAccount account = new BankAccount();
+        account.deposit(-50);
+    }
 }
