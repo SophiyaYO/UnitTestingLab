19b31094a433c894c239cb1b12e2d7a006ec85a8
SophYo <sophiya.dimitrova@gmail.com>
2019-07-31 23:31:34 +0300

    code added to first method

--- src/test/java/DummyTests.java
+++ src/test/java/DummyTests.java
@@ -1,10 +1,19 @@
+import org.junit.Assert;
 import org.junit.Test;
+import rpg_lab.Axe;
+import rpg_lab.Dummy;
 
 public class DummyTests {
 
     @Test
     public void dummyShouldLosesHealthAttacked() {
-        //todo
+        Axe axe = new Axe(10, 10);
+        Dummy target = new Dummy(20,10);
+
+        axe.attack(target);
+
+        Assert.assertEquals(10, target.getHealth());
+
     }
 
     @Test
