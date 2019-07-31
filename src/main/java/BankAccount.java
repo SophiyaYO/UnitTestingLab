a5ddeae88b83689d028b9d36d025fb295c94a779
SophYo <sophiya.dimitrova@gmail.com>
2019-07-31 22:43:23 +0300

    initial commit with field, constructor and methods in

---
+++ src/main/java/BankAccount.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class BankAccount {
+    List<Double> money;
+
+    public BankAccount() {
+        this.money = new ArrayList<>();
+    }
+
+    public void deposit(double amount) {
+        this.money.add(amount);
+    }
+
+    public double getBalance() {
+        return this.money.stream()
+                .mapToDouble(s-> s)
+                .sum();
+
+    }
+
+}
