44bc856bbf7a2c5934ae85cf92af138da8b2f2e1
SophYo <sophiya.dimitrova@gmail.com>
2019-08-01 11:47:13 +0300

    method additions

--- src/test/java/HeroTest.java
+++ src/test/java/HeroTest.java
@@ -1,10 +1,13 @@
 import org.junit.Test;
 import rpg_lab.Target;
+import rpg_lab.Weapon;
 
 public class HeroTest {
 
     private static final int DEFAULT_EXPERIENCE = 10;
-    private static final int DEFAULT_TARGET_HEALTH = 10;
+    private static final int DEFAULT_TARGET_HEALTH = 0;
+    private static final int DEFAULT_WEAPON_ATTACK_POINTS = 10;
+    private static final int DEFAULT_WEAPON_DURABILITY_POINTS = 0;
     private static final boolean DEFAULT_IS_TARGET_DEAD = Boolean.TRUE;
 
     @Test
@@ -29,6 +32,23 @@ public class HeroTest {
             @Override
             public boolean isDead() {
                 return DEFAULT_IS_TARGET_DEAD;
+            }
+        };
+
+        Weapon weapon = new Weapon() {
+            @Override
+            public int getAttackPoints() {
+                return DEFAULT_WEAPON_ATTACK_POINTS;
+            }
+
+            @Override
+            public int getDurabilityPoints() {
+                return DEFAULT_WEAPON_DURABILITY_POINTS;
+            }
+
+            @Override
+            public void attack(Target target) {
+
             }
         };
 
