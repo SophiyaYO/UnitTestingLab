19c7dc4756082f7c36d54ff201bdf12f31abbf08
SophYo <sophiya.dimitrova@gmail.com>
2019-08-01 10:58:37 +0300

    constants added

--- src/test/java/DummyTests.java
+++ src/test/java/DummyTests.java
@@ -5,14 +5,18 @@ import rpg_lab.Axe;
 import rpg_lab.Dummy;
 
 public class DummyTests {
+    private static final int DEFAULT_DUMMY_HEALTH = 20;
+    private static final int DEFAULT_DUMMY_EXPERIENCE = 10;
+    private static final int DEFAULT_AXE_ATTACK = 10;
+    private static final int DEFAULT_AXE_DURABILITY = 10;
 
     private Dummy target;
     private Axe axe;
 
     @Before
     public void initValues() {
-        this.target = new Dummy(20,10);
-        this.axe = new Axe(10, 10);
+        this.target = new Dummy(DEFAULT_DUMMY_HEALTH,DEFAULT_DUMMY_EXPERIENCE);
+        this.axe = new Axe(DEFAULT_AXE_ATTACK, DEFAULT_AXE_DURABILITY);
     }
 
     @Test
